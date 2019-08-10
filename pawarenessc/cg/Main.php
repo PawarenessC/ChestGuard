1919e845d5ea2c1c148418eb54f70c4d267da8b8
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-10 19:24:29 +0900

    今日はここまで

--- pawarenessc/cg/Main.php
+++ pawarenessc/cg/Main.php
@@ -16,4 +16,64 @@ use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\event\block\BlockPlaceEvent;
 
-Class Main extends pluginBase implements Listener{)
+use pocketmine\block\BlockIds as BID;
+
+Class Main extends pluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getLogger()->info("=========================");
+		$this->getLogger()->info("ChestGuardを読み込みました");
+		$this->getLogger()->info("製作者: PawarenessC");
+		$this->getLogger()->info("ライセンス: NYSL Version 0.9982");
+		$this->getLogger()->info("http://www.kmonos.net/nysl/");
+		$this->getLogger()->info("バージョン:v{$this->getDescription()->getVersion()}");
+		$this->getLogger()->info("=========================");
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->Config();
+	}
+	
+	public function onDisable(){
+		$this->getLogger()->info("=========================");
+		$this->getLogger()->info("ChestGuardを停止");
+		$this->getLogger()->info("製作者: PawarenessC");
+		$this->getLogger()->info("バージョン:v{$this->getDescription()->getVersion()}");
+		$this->getLogger()->info("=========================");
+	}
+	
+	public function PlaceChest(BlockPlaceEvent $event){
+		if($event->getBlock()->getId() == BID::CHEST){
+			$player = $event->getPlayer();
+			$name = $player->getName();
+			$block = $event->getBlock();
+			$x = $block->x;
+			$y = $block->y;
+			$z = $block->z;
+			$data = $x.$y.$z;
+			$this->chest->set($data,[
+			"name"=>$name,
+			"status"=>"guard",
+			]);
+		}
+	}
+	
+	public function BreakChest(BlockBreakEvent $event){
+		if($event->getBlock()->getId() == BID::CHEST){
+			$player = $event->getPlayer();
+			$name = $player->getName();
+			$block = $event->getBlock();
+			$x = $block->x;
+			$y = $block->y;
+			$z = $block->z;
+			$data = $x.$y.$z;
+			if($this->chest->exists($data)){ $this->config->remove($data); }
+		}
+	}
+	
+	/*public function OpenChest(PlayerInteractEvent $event){
+		if($id = $event->getBlock()->getId() == BID::CHEST){
+		}
+	}*/
+}
+	
+	
+		
