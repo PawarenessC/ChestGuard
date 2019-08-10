913b54b8559ccfb206b5feefe1c6e919497bd79a
PawarenessC <35727789+PawarenessC@users.noreply.github.com>
2019-08-10 18:26:14 +0900

    Create Main.php

---
+++ pawarenessc/cg/Main.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace pawarenessc\dc;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\block\BlockPlaceEvent;
+
+Class Main extends pluginBase implements Listener{)
